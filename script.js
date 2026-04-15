021ae9a85596f6dfe4a188c2d863405742e76f9e
LacritaProducciones <lacrita_producciones@outlook.com>
2026-04-15 11:18:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function crearLirio() {
+    const lirio = document.createElement("img");
+    lirio.src = "lirio.png";
+    lirio.classList.add("lirio");
+
+    lirio.style.left = Math.random() * window.innerWidth + "px";
+    lirio.style.animationDuration = (5 + Math.random() * 5) + "s";
+
+    document.body.appendChild(lirio);
+
+    setTimeout(() => {
+        lirio.remove();
+    }, 10000);
+}
+
+setInterval(crearLirio, 800);
